16607a9bcd8eb710bc0bc1710b7b18e103f8f4b5
admin <admin@ADMIN-8Q2MKM71H>
2013-06-06 15:15:35 +0900

    yuyakase

--- src/com/example/hellogithub/MainActivity.java
+++ src/com/example/hellogithub/MainActivity.java
@@ -9,13 +9,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		// autohn
 		// hogehoge
 		// こうやってコードを共有する
 		//Boys, be ambitious !
+		// yuyakase
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
